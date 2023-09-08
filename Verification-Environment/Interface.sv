5cf3750c2e20a60713bd4a3f4b634aa34754bf77
Srirangarajan™ <106532953+Eloquencere@users.noreply.github.com>
2023-09-08 16:27:53 +0530

    Delete Verification-Environment directory

--- Verification-Environment/Interface.sv
+++
@@ -1,8 +0,0 @@
-`timescale 1ns / 1ps
-
-interface DUT_interface();
-    logic [15:0]a,b;
-    logic cin;
-    logic [15:0]sum;
-    logic cout;
-endinterface
--- Verification-Environment/Packet.sv
+++
@@ -1,6 +0,0 @@
-class packet;
-    randc bit [15:0]a, b;
-    randc bit cin;
-    bit [15:0]sum;
-    bit cout;
-endclass
