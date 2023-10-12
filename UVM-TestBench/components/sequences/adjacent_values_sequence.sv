2bf13c9aa7d28a5725a2c7de16649f4b4a7fcc0b
Srirangarajan™ <106532953+Eloquencere@users.noreply.github.com>
2023-10-12 16:37:06 +0530

    Add files via upload

---
+++ UVM-TestBench/components/sequences/adjacent_values_sequence.sv
@@ -0,0 +1,26 @@
+class adjacent_values_sequence extends uvm_sequence;
+    `uvm_object_utils(adjacent_values_sequence)
+    
+    function new(string name = "adjacent_values_sequence");
+        super.new(name);
+    endfunction
+    
+    int total_packets = 10;
+    adder_packet packet_to_sequencer;
+    
+    virtual task pre_body();
+        packet_to_sequencer = adder_packet::type_id::create("packet_to_sequencer");
+    endtask
+    
+    virtual task body();
+        repeat(total_packets)
+        begin
+            start_item(packet_to_sequencer);
+            
+            assert(packet_to_sequencer.randomize() with {(a-b) < 40;})
+            else `uvm_fatal(get_name(), "Unable to randomize")
+            
+            finish_item(packet_to_sequencer);
+        end
+    endtask
+endclass
