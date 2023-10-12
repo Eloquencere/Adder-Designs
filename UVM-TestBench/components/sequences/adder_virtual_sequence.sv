6610f1b6f80eeaa2b08f3f96def829c85f765fcd
Srirangarajan™ <106532953+Eloquencere@users.noreply.github.com>
2023-10-12 16:36:19 +0530

    Update adder_virtual_sequence.sv

--- UVM-TestBench/components/sequences/adder_virtual_sequence.sv
+++ UVM-TestBench/components/sequences/adder_virtual_sequence.sv
@@ -12,6 +12,7 @@ class adder_virtual_sequence extends uvm_sequence;
     carry_propagation_sequence crry_sqnc;
     overflow_sequence ovrfl_sqnc;
     underflow_sequence undrfl_sqnc;
+    adjacent_values_sequence adjcnt_sqnc;
     random_no_constraint_sequence rand_sqnc;
     
     uvm_sequence test_case[$];
@@ -23,14 +24,16 @@ class adder_virtual_sequence extends uvm_sequence;
         test_case.push_back(carry_propagation_sequence::type_id::create("crry_sqnc"));
         test_case.push_back(overflow_sequence::type_id::create("ovrfl_sqnc"));
         test_case.push_back(underflow_sequence::type_id::create("undrfl_sqnc"));
+        test_case.push_back(adjacent_values_sequence::type_id::create("adjcnt_sqnc"));
         test_case.push_back(random_no_constraint_sequence::type_id::create("rand_sqnc"));
     endtask
-    
+
     // seed no. = 2807422414
+    
     virtual task body();
         foreach(test_case[i])
         begin
-            $display("Started Sequence %d", i);
+            $display("Started Sequence %0d", i);
             foreach(p_sequencer.sqncr[j])
             fork
                 int temp = j;
