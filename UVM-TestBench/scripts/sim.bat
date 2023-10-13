fab9cfc2ae5db802c6a5af074221e414107315ea
Srirangarajan™ <106532953+Eloquencere@users.noreply.github.com>
2023-10-13 21:27:08 +0530

    Update sim.bat

--- UVM-TestBench/scripts/sim.bat
+++ UVM-TestBench/scripts/sim.bat
@@ -1,14 +1,19 @@
+@echo off
+cls
+python3 credits.py
+cd ../sim_data/
+@echo on
+vlog -f "../components/top/verification_components_filelist.f" +VERBOSITY_LOW -timescale 1ns/1ps -coveropt 3 +cover +acc
+vsim top -sv_seed random -logfile log_file.txt -voptargs=+acc=npr -coverage -vopt -c -do "../scripts/run.do" +UVM_NO_RELNOTES
+vcover report -details -html code_coverage.ucdb
+@echo off
+start covhtmlreport/index.html
+start log_file.txt
+cd ../scripts/
+
+
 @REM cd ../sim_data/
 @REM cls
-@REM vsim -c -do "../scripts/run.do"
+@REM vlog -f "../components/top/verification_components_filelist.f" +VERBOSITY_LOW -R -sv_seed random -c -do "../scripts/run.do" -solvefaildebug=2
 @REM @echo off
-@REM start covhtmlreport/index.html
-@REM start notepads log_file.txt
 @REM cd ../scripts/
-
-
-cd ../sim_data/
-cls
-vlog -f "../components/top/xyz.f" +VERBOSITY_LOW -R -sv_seed random -c -do "../scripts/run.do"
-@echo off
-cd ../scripts/
