2056e2b63b84606975f9214d9579962d8af8de28
Srirangarajan™ <106532953+Eloquencere@users.noreply.github.com>
2023-10-13 21:39:44 +0530

    Add files via upload

---
+++ UVM-TestBench/components/top/verification_components_filelist.f
@@ -0,0 +1,8 @@
+-f ../../designs/designs_under_test_filelist.f
+
+../components/interface/adder_interface.sv
+../components/utilities/adder_colours_pkg.sv
+../components/utilities/adder_testbench_constants_pkg.sv
+../components/sequences/adder_sequences_pkg.sv
+../components/testbench/adder_testbench_pkg.sv
+../components/top/top.sv
