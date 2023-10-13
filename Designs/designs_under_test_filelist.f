045942b2b83192f21a6ce87ac25e10efec9deb50
Srirangarajan™ <106532953+Eloquencere@users.noreply.github.com>
2023-10-13 20:36:38 +0530

    Add files via upload

---
+++ Designs/designs_under_test_filelist.f
@@ -0,0 +1,8 @@
+// include this directory when compiling to remove the ../../
+
+../../designs/CIA_16-bit.v
+../../designs/CLA_16-bit.v
+../../designs/CSelA_16-bit.v
+../../designs/CSkA_16-bit.v
+../../designs/RCA_16-bit.v
+../../designs/MCCA_16-bit.v
