c36fad818181ddd01de302997eb1d4c7d7ea8726
Srirangarajan™ <106532953+Eloquencere@users.noreply.github.com>
2023-10-13 21:28:58 +0530

    Add files via upload

---
+++ UVM-TestBench/scripts/credits.py
@@ -0,0 +1,8 @@
+from pyfiglet import figlet_format
+import time
+
+print(figlet_format("UVM Punks", font="doh", width=90))
+
+print("Made with \033[91m❤\033[0m by Eloquencere & Spacebiz24")
+
+time.sleep(7)
