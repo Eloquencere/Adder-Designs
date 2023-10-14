8c716d7c494f503c4bf78201b8ac340b3418398a
Srirangarajan™ <106532953+Eloquencere@users.noreply.github.com>
2023-10-14 08:42:01 +0530

    Update sim.bat

--- UVM-TestBench/scripts/sim.bat
+++ UVM-TestBench/scripts/sim.bat
@@ -2,11 +2,9 @@
 cls
 python3 credits.py
 cd ../sim_data/
-@echo on
 vlog -f "../components/top/verification_components_filelist.f" +VERBOSITY_LOW -timescale 1ns/1ps -coveropt 3 +cover +acc
 vsim top -sv_seed random -logfile log_file.txt -voptargs=+acc=npr -coverage -vopt -c -do "../scripts/run.do" +UVM_NO_RELNOTES
 vcover report -details -html code_coverage.ucdb
-@echo off
 start covhtmlreport/index.html
 start log_file.txt
 cd ../scripts/
