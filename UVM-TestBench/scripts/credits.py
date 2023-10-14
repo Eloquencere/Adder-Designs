60115a461214c150f4e672717b7ab2675cdaa0ed
Srirangarajan™ <106532953+Eloquencere@users.noreply.github.com>
2023-10-14 12:10:04 +0530

    Update credits.py

--- UVM-TestBench/scripts/credits.py
+++ UVM-TestBench/scripts/credits.py
@@ -5,14 +5,11 @@ import time
 
 def display_countdown(text, seconds):
     print(f"{text} in {seconds} seconds", end="", flush=True)
-    for i in range(seconds, 0, -1):
+    for i in range(seconds, -1, -1):
         sys.stdout.write("\r")
         sys.stdout.write("\033[K")
         print(f"{text} in {i} seconds", end="", flush=True)
         time.sleep(1)
-    sys.stdout.write("\r")
-    sys.stdout.write("\033[K")
-    print(f"{text} in 0 seconds")
 
 
 print(pyfiglet.figlet_format("Bit Bots", font="georgia11", width=700))
