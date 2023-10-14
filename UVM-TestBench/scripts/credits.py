7526ee4630bb290d4a06dff7f483b3063e6f726d
Srirangarajan™ <106532953+Eloquencere@users.noreply.github.com>
2023-10-14 08:41:31 +0530

    Update credits.py

--- UVM-TestBench/scripts/credits.py
+++ UVM-TestBench/scripts/credits.py
@@ -1,8 +1,24 @@
-from pyfiglet import figlet_format
+import sys
+import pyfiglet
 import time
 
-print(figlet_format("UVM Punks", font="doh", width=90))
 
-print("Made with \033[91m❤\033[0m by Sriranga(github.com/Eloquencere/) & Siddhaanth(github.com/spacebiz24/)")
+def display_countdown(text, seconds):
+    print(f"{text} in {seconds} seconds", end="", flush=True)
+    for i in range(seconds, 0, -1):
+        sys.stdout.write("\r")
+        sys.stdout.write("\033[K")
+        print(f"{text} in {i} seconds", end="", flush=True)
+        time.sleep(1)
+    sys.stdout.write("\r")
+    sys.stdout.write("\033[K")
+    print(f"{text} in 0 seconds")
 
-time.sleep(7)
+
+print(pyfiglet.figlet_format("Bit Bots", font="georgia11", width=700))
+
+print(
+    "Made with \033[91m❤\033[0m by Sriranga(github.com/Eloquencere/) & Siddhaanth(github.com/spacebiz24/)"
+)
+print("\n")
+display_countdown("Initialising Verification Environment", 7)
