4a67f059b62a19ceda933e1ae552e25b70665a93
Srirangarajan™ <106532953+Eloquencere@users.noreply.github.com>
2023-10-16 18:17:24 +0530

    Update adder_virtual_sequence.sv

--- UVM-TestBench/components/sequences/adder_virtual_sequence.sv
+++ UVM-TestBench/components/sequences/adder_virtual_sequence.sv
@@ -8,24 +8,23 @@ class adder_virtual_sequence extends uvm_sequence;
     
     uvm_sequence test_case[$];
     
-    basic_operation_sequence basic_sqnc;
-    single_bit_sequence sngl_sqnc;
-    zero_propagation_sequence zro_sqnc;
-    carry_propagation_sequence crry_sqnc;
-    overflow_sequence ovrfl_sqnc;
-    underflow_sequence undrfl_sqnc;
-    adjacent_values_sequence adjcnt_sqnc;
-    random_no_constraint_sequence rand_sqnc;
-    
     virtual task pre_body();
-        test_case.push_back(basic_operation_sequence::type_id::create("basic_sqnc"));
-        test_case.push_back(single_bit_sequence::type_id::create("sngl_sqnc"));
-        test_case.push_back(zero_propagation_sequence::type_id::create("zro_sqnc"));
-        test_case.push_back(carry_propagation_sequence::type_id::create("crry_sqnc"));
-        test_case.push_back(overflow_sequence::type_id::create("ovrfl_sqnc"));
-        test_case.push_back(underflow_sequence::type_id::create("undrfl_sqnc"));
-        test_case.push_back(adjacent_values_sequence::type_id::create("adjcnt_sqnc"));
-        test_case.push_back(random_no_constraint_sequence::type_id::create("rand_sqnc"));
+        basic_operation_sequence basic_sqnc = basic_operation_sequence::type_id::create("basic_sqnc");
+        single_bit_sequence sngl_sqnc = single_bit_sequence::type_id::create("sngl_sqnc");
+        zero_propagation_sequence zro_sqnc = zero_propagation_sequence::type_id::create("zro_sqnc");
+        carry_propagation_sequence crry_sqnc = carry_propagation_sequence::type_id::create("crry_sqnc");
+        overflow_sequence ovrfl_sqnc = overflow_sequence::type_id::create("ovrfl_sqnc");
+        underflow_sequence undrfl_sqnc = underflow_sequence::type_id::create("undrfl_sqnc");
+        adjacent_values_sequence adjcnt_sqnc = adjacent_values_sequence::type_id::create("adjcnt_sqnc");
+        random_no_constraint_sequence rand_sqnc = random_no_constraint_sequence::type_id::create("rand_sqnc");
+        test_case.push_back(basic_sqnc);
+        test_case.push_back(sngl_sqnc);
+        test_case.push_back(zro_sqnc);
+        test_case.push_back(crry_sqnc);
+        test_case.push_back(ovrfl_sqnc);
+        test_case.push_back(undrfl_sqnc);
+        test_case.push_back(adjcnt_sqnc);
+        test_case.push_back(rand_sqnc);
     endtask
     
     virtual task body();
