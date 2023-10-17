9fa64a83e4b48724aa49e198038c703e0aee07c8
Srirangarajan™ <106532953+Eloquencere@users.noreply.github.com>
2023-10-18 00:21:53 +0530

    Update adder_report_server.sv

--- UVM-TestBench/components/report_server/adder_report_server.sv
+++ UVM-TestBench/components/report_server/adder_report_server.sv
@@ -14,13 +14,11 @@ class adder_report_server extends uvm_report_server;
     adder_colours_pkg::colour_t colour;
     
     if(colour_enable)
-    begin
         case(sev.name())
             "UVM_INFO": colour = adder_colours_pkg::BLUE;
             "UVM_WARNING": colour = adder_colours_pkg::YELLOW;
             "UVM_ERROR", "UVM_FATAL": colour = adder_colours_pkg::RED;
         endcase
-    end
     
     return adder_colours_pkg::colourise(colour_enable, colour, $sformatf("%s %s(%0d) @ %0t: %s [%s] %s",
                         sev.name(), filename.substr(27, filename.len()-1), line, $time, name, id, message));
