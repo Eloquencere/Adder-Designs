716a1020d887de99d67cb25c75f4b75aa5cddf23
Srirangarajan™ <106532953+Eloquencere@users.noreply.github.com>
2023-10-18 11:54:04 +0530

    Update adder_report_server.sv

--- UVM-TestBench/components/report_server/adder_report_server.sv
+++ UVM-TestBench/components/report_server/adder_report_server.sv
@@ -10,17 +10,15 @@ class adder_report_server extends uvm_report_server;
     );
     uvm_severity_type sev = uvm_severity_type'(severity);
     
-    bit colour_enable = 1;
-    adder_colours_pkg::colour_t colour;
-    
-    if(colour_enable)
+    adder_colours_pkg::colour_t colour = adder_colours_pkg::WHITE;
+    if(adder_testbench_constants_pkg::colourise_report_message)
         case(sev.name())
             "UVM_INFO": colour = adder_colours_pkg::BLUE;
             "UVM_WARNING": colour = adder_colours_pkg::YELLOW;
             "UVM_ERROR", "UVM_FATAL": colour = adder_colours_pkg::RED;
         endcase
     
-    return adder_colours_pkg::colourise(colour_enable, colour, $sformatf("%s %s(%0d) @ %0t: %s [%s] %s",
+    return adder_colours_pkg::colourise(colour, $sformatf("%s %s(%0d) @ %0t: %s [%s] %s",
                         sev.name(), filename.substr(27, filename.len()-1), line, $time, name, id, message));
     endfunction
 endclass
