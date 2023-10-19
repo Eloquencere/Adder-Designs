36aa936ee074a6d6ea93135ca0803fde1abd84b7
Srirangarajan™ <106532953+Eloquencere@users.noreply.github.com>
2023-10-19 08:37:32 +0530

    Update adder_coverage_collector.sv

--- UVM-TestBench/components/environment/adder_coverage_collector.sv
+++ UVM-TestBench/components/environment/adder_coverage_collector.sv
@@ -34,12 +34,12 @@ class adder_coverage_collector extends uvm_subscriber#(adder_packet);
     adder_environment_config env_cfg;
     
     virtual function void build_phase(uvm_phase phase);
-        `uvm_info(get_type_name(), "Started build_phase", UVM_FULL)
+        `uvm_info(get_type_name(), $sformatf("Started %s_phase", phase.get_name()), UVM_FULL)
         
         assert(uvm_config_db#(adder_environment_config)::get(this, "scrbrd", "env_cfg", env_cfg))
         else `uvm_fatal(get_type_name(), "Failed to get environment config")
         
-        `uvm_info(get_type_name(), "Finished build_phase", UVM_FULL)
+        `uvm_info(get_type_name(), $sformatf("Finished %s_phase", phase.get_name()), UVM_FULL)
     endfunction
     
     virtual function void write(adder_packet t);
@@ -48,11 +48,11 @@ class adder_coverage_collector extends uvm_subscriber#(adder_packet);
     endfunction
     
     virtual function void report_phase(uvm_phase phase);
-        `uvm_info(get_type_name(), "Started report_phase", UVM_HIGH)
+        `uvm_info(get_type_name(), $sformatf("Started %s_phase", phase.get_name()), UVM_HIGH)
         
         if(env_cfg.need_coverage)
             `uvm_info(get_type_name(), $sformatf("Instance Coverage = %f", this.set.get_inst_coverage()), UVM_NONE)
         
-        `uvm_info(get_type_name(), "Finished report_phase", UVM_HIGH)
+        `uvm_info(get_type_name(), $sformatf("Finished %s_phase", phase.get_name()), UVM_HIGH)
     endfunction
 endclass
