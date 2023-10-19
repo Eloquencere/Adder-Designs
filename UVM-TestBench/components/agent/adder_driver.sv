756f29b44d9fa98d09a418196b37fb80888fa515
Srirangarajan™ <106532953+Eloquencere@users.noreply.github.com>
2023-10-19 08:36:34 +0530

    Update adder_driver.sv

--- UVM-TestBench/components/agent/adder_driver.sv
+++ UVM-TestBench/components/agent/adder_driver.sv
@@ -10,25 +10,25 @@ class adder_driver extends uvm_driver#(adder_packet);
     virtual adder_interface.drvr_modprt drvr_vintrf;
     
     virtual function void build_phase(uvm_phase phase);
-        `uvm_info(get_name(), "Started build_phase", UVM_FULL)
+        `uvm_info(get_name(), $sformatf("Started %s_phase", phase.get_name()), UVM_FULL)
         
         assert(uvm_config_db#(adder_agent_config)::get(this, "drvr", "agnt_cfg", agnt_cfg))
         else `uvm_fatal(get_name(), "Failed to get agent config")
         
-        `uvm_info(get_name(), "Finished build_phase", UVM_FULL)
+        `uvm_info(get_name(), $sformatf("Finished %s_phase", phase.get_name()), UVM_FULL)
     endfunction
     
     virtual function void connect_phase(uvm_phase phase);
-        `uvm_info(get_type_name(), "Started connect_phase", UVM_FULL)
+        `uvm_info(get_name(), $sformatf("Started %s_phase", phase.get_name()), UVM_FULL)
         
         assert(uvm_config_db#(virtual adder_interface)::get(this, "drvr", $sformatf("%s_Interface", agnt_cfg.dut_name), drvr_vintrf))
-        else `uvm_fatal(get_type_name(), "Failed to get a handle to the interface")
+        else `uvm_fatal(get_name(), "Failed to get a handle to the interface")
         
-        `uvm_info(get_type_name(), "Finished connect_phase", UVM_FULL)
+        `uvm_info(get_name(), $sformatf("Finished %s_phase", phase.get_name()), UVM_FULL)
     endfunction
     
     virtual task run_phase(uvm_phase phase);
-        `uvm_info(get_name(), "Started run_phase", UVM_MEDIUM)
+        `uvm_info(get_name(), $sformatf("Started %s_phase", phase.get_name()), UVM_MEDIUM)
         
         forever
         begin
@@ -44,14 +44,14 @@ class adder_driver extends uvm_driver#(adder_packet);
             #2;
         end
         
-        `uvm_info(get_name(), "Finished run_phase", UVM_MEDIUM)
+        `uvm_info(get_name(), $sformatf("Finished %s_phase", phase.get_name()), UVM_MEDIUM)
     endtask
     
     virtual function void report_phase(uvm_phase phase);
-        `uvm_info(get_type_name(), "Started report_phase", UVM_MEDIUM)
+        `uvm_info(get_name(), $sformatf("Started %s_phase", phase.get_name()), UVM_MEDIUM)
         
         `uvm_info(get_full_name(), $sformatf("Packets driven = %0d", agnt_cfg.drvr_trans_count), UVM_LOW)
         
-        `uvm_info(get_type_name(), "Finished report_phase", UVM_MEDIUM)
+        `uvm_info(get_name(), $sformatf("Finished %s_phase", phase.get_name()), UVM_MEDIUM)
     endfunction
 endclass
