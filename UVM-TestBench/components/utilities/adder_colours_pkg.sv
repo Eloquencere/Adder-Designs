1ed845b3e8f69f1ce61312a9e685e8a1f6f114d2
Srirangarajan™ <106532953+Eloquencere@users.noreply.github.com>
2023-10-20 08:29:38 +0530

    Update adder_colours_pkg.sv

--- UVM-TestBench/components/utilities/adder_colours_pkg.sv
+++ UVM-TestBench/components/utilities/adder_colours_pkg.sv
@@ -2,10 +2,10 @@ package adder_colours_pkg;
     typedef enum {WHITE, BLUE, YELLOW, RED} colour_t;
     string fg_format[colour_t] = 
     '{
-        WHITE: "\033[37m",
-        BLUE:"\033[94m",
-        YELLOW:"\033[33m",
-        RED:"\033[31m"
+        WHITE : "\033[37m",
+        BLUE : "\033[94m",
+        YELLOW : "\033[33m",
+        RED : "\033[31m"
     };
     
     function string colourise(colour_t colour, string text);
