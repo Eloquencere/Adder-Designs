5fcb46d47124293b9b374de7002c21c0e517c23d
Srirangarajan™ <106532953+Eloquencere@users.noreply.github.com>
2023-10-21 08:50:01 +0530

    Update designs_under_test_filelist.f

--- Designs/designs_under_test_filelist.f
+++ Designs/designs_under_test_filelist.f
@@ -1,8 +1,6 @@
-// include this directory when compiling to remove the ../../
-
-../../designs/Carry-Increment/*
-../../designs/Carry-Look-Ahead/*
-../../designs/Carry-Select/*
-../../designs/Carry-Skip/*
-../../designs/Ripple-Carry/*
-../../designs/Manchester-Carry-Chain/*
+../../Designs/Carry-Increment/*
+../../Designs/Carry-Look-Ahead/*
+../../Designs/Carry-Select/*
+../../Designs/Carry-Skip/*
+../../Designs/Ripple-Carry/*
+../../Designs/Manchester-Carry-Chain/*
