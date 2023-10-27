aa4f1766175e5bc30275c62d8c469e1dc08976bc
Srirangarajan™ <106532953+Eloquencere@users.noreply.github.com>
2023-10-27 21:08:55 +0530

    Update verification_components_filelist.f

--- UVM-TestBench/components/top/verification_components_filelist.f
+++ UVM-TestBench/components/top/verification_components_filelist.f
@@ -1,8 +1,8 @@
 -f ../../designs/designs_under_test_filelist.f
 
 ../components/interface/adder_interface.sv
-../components/utilities/adder_colours_pkg.sv
-../components/utilities/adder_testbench_constants_pkg.sv
-../components/sequences/adder_sequences_pkg.sv
-../components/testbench/adder_testbench_pkg.sv
+../components/utilities/adder_colours_pkg.svh
+../components/utilities/adder_testbench_constants_pkg.svh
+../components/sequences/adder_sequences_pkg.svh
+../components/testbench/adder_testbench_pkg.svh
 ../components/top/top.sv
