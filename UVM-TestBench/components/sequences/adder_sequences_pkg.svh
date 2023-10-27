72bafcd6c3da20e9d4d2d63aa8eff35a901965e9
Srirangarajan™ <106532953+Eloquencere@users.noreply.github.com>
2023-10-27 21:07:58 +0530

    Add files via upload

---
+++ UVM-TestBench/components/sequences/adder_sequences_pkg.svh
@@ -0,0 +1,15 @@
+package adder_sequences_pkg;
+	import uvm_pkg::*;
+	`include "uvm_macros.svh"
+	
+	`include "sequence_item/adder_sequence_item.sv"
+	
+	`include "basic_operation_sequence.sv"
+	`include "single_bit_sequence.sv"
+	`include "zero_propagation_sequence.sv"
+	`include "carry_propagation_sequence.sv"
+	`include "overflow_sequence.sv"
+	`include "underflow_sequence.sv"
+	`include "adjacent_values_sequence.sv"
+	`include "random_no_constraint_sequence.sv"
+endpackage
