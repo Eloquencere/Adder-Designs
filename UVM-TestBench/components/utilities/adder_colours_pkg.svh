e15db4ac42ea1a64c011a0520cb96e102e98f5d3
Srirangarajan™ <106532953+Eloquencere@users.noreply.github.com>
2023-10-27 21:06:22 +0530

    Add files via upload

---
+++ UVM-TestBench/components/utilities/adder_colours_pkg.svh
@@ -0,0 +1,15 @@
+package adder_colours_pkg;
+    typedef enum {WHITE, BLUE, YELLOW, RED, GREEN} colour_t;
+    string fg_format[colour_t] = 
+    '{
+        WHITE : "\033[37m",
+        BLUE : "\033[94m",
+        YELLOW : "\033[33m",
+        RED : "\033[31m",
+        GREEN : "\033[32m"
+    };
+    
+    function string colourise(colour_t colour, string text);
+        return $sformatf("%s%s\033[0m", fg_format[colour], text);
+    endfunction
+endpackage
