e80ef4eb7e2f514ec4ef0bc7edccb057b038dbbe
Srirangarajan™ <106532953+Eloquencere@users.noreply.github.com>
2023-11-06 00:33:14 +0530

    Update adder_virtual_sequencer.sv

--- UVM-TestBench/components/environment/adder_virtual_sequencer.sv
+++ UVM-TestBench/components/environment/adder_virtual_sequencer.sv
@@ -10,6 +10,7 @@ class adder_virtual_sequencer extends uvm_sequencer;
     virtual function void build_phase(uvm_phase phase);
         `uvm_info(get_name(), $sformatf("Started %s_phase", phase.get_name()), UVM_FULL)
         
+        // Creating a sequencer handle for every agent to be created
         sqncr = new[adder_testbench_constants_pkg::dut_list.size()];
         
         `uvm_info(get_name(), $sformatf("Finished %s_phase", phase.get_name()), UVM_FULL)
