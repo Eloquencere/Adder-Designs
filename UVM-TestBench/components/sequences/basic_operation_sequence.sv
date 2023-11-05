8310991871ca5db608b1f35c9c3ebe871479d7d7
Srirangarajan™ <106532953+Eloquencere@users.noreply.github.com>
2023-11-06 00:35:19 +0530

    Update basic_operation_sequence.sv

--- UVM-TestBench/components/sequences/basic_operation_sequence.sv
+++ UVM-TestBench/components/sequences/basic_operation_sequence.sv
@@ -14,6 +14,7 @@ class basic_operation_sequence extends uvm_sequence;
         begin
             start_item(packet_to_sequencer);
             
+            // Creating a -> +ve & b -> +ve cases
             assert(packet_to_sequencer.randomize() with {a[15] == 0 && b[15] == 0;})
             else `uvm_fatal(get_name(), "Unable to randomize")
             
@@ -23,6 +24,7 @@ class basic_operation_sequence extends uvm_sequence;
         begin
             start_item(packet_to_sequencer);
             
+            // Creating a -> +ve or -ve & b -> +ve or -ve cases
             assert(packet_to_sequencer.randomize() with {a[15] == 1 || b[15] == 1;})
             else `uvm_fatal(get_name(), "Unable to randomize") 
             
