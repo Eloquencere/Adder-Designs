6d9ea3cc1d7674b1a29664e9dc7acec9766ac4c5
Srirangarajan™ <106532953+Eloquencere@users.noreply.github.com>
2023-11-06 00:35:49 +0530

    Update zero_propagation_sequence.sv

--- UVM-TestBench/components/sequences/zero_propagation_sequence.sv
+++ UVM-TestBench/components/sequences/zero_propagation_sequence.sv
@@ -26,6 +26,7 @@ class zero_propagation_sequence extends uvm_sequence;
         begin
             start_item(packet_to_sequencer);
             
+            // Creating (random + (-random) = 0) cases
             assert(packet_to_sequencer.randomize() with {a == -b ^^ b == -a;})
             else `uvm_fatal(get_name(), "Unable to randomize")
             
@@ -35,6 +36,7 @@ class zero_propagation_sequence extends uvm_sequence;
         begin
             start_item(packet_to_sequencer);
             
+            // Creating random + 0 = random or 0 + random = random cases
             assert(packet_to_sequencer.randomize() with {(a == 0) ^^ (b == 0);})
             else `uvm_fatal(get_name(), "Unable to randomize")
             
