6f24fdc0c533a62e9d64ceaf3fad4ec209c73855
Srirangarajan™ <106532953+Eloquencere@users.noreply.github.com>
2023-11-07 19:13:38 +0530

    Update adder_testbench_constants_pkg.svh

--- UVM-TestBench/components/utilities/adder_testbench_constants_pkg.svh
+++ UVM-TestBench/components/utilities/adder_testbench_constants_pkg.svh
@@ -2,7 +2,7 @@ package adder_testbench_constants_pkg;
     parameter colourise_report_message = 1;
     
     parameter string dut_list[] = 
-    {
+    '{
         "CIAxbit",
         "CLAxbit",
         "CSelAxbit",
