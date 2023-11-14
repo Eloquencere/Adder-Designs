b217e9db16ba43423b235b4f31e21a97895ec383
Srirangarajan™ <106532953+Eloquencere@users.noreply.github.com>
2023-11-14 20:20:25 +0530

    Update adder_test_config.sv

--- UVM-TestBench/components/testbench/adder_test_config.sv
+++ UVM-TestBench/components/testbench/adder_test_config.sv
@@ -1,12 +1,7 @@
 class adder_test_config extends uvm_object;
     `uvm_object_utils(adder_test_config)
     
-    adder_report_server srvr;
-    
     function new(string name = "adder_test_config");
         super.new(name);
-        srvr = new();
-        // Assign custom report server to global report server
-        uvm_report_server::set_server(srvr);
     endfunction
 endclass
