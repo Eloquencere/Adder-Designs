4874c32f0154cb5b4fafe1ab30a822678dec7a88
Srirangarajan™ <106532953+Eloquencere@users.noreply.github.com>
2024-09-17 11:53:31 +0530

    Update MCCA_16-bit(dynamic).v

--- Designs/Manchester-Carry-Chain/MCCA_16-bit(dynamic).v
+++ Designs/Manchester-Carry-Chain/MCCA_16-bit(dynamic).v
@@ -1,39 +1,71 @@
-module AdderBlock64bit(sum,cout,a,b,cin);
-parameter size = 16; //size >= 4;
-    output [size-1:0]sum;
+module tb;
+    parameter integer SIZE = 16;
+    reg [SIZE-1:0]clk;
+    reg [SIZE-1:0]a, b;
+    reg cin;
+    wire [SIZE-1:0]sum;
+    wire cout;
+    AdderBlock64bit dut(.sum(sum),.cout(cout),.a(a),.b(b),.cin(cin),.clk(clk));
+    initial
+        $monitor("time -> %t, clk -> %d, a -> %b, b -> %b, cin -> %b, sum -> %b", $time, clk,a, b, cin, sum);
+    initial begin
+        clk = {SIZE-1{1'b0}};
+        forever #10 clk = ~clk;
+    end
+    initial begin
+        a=0;b=0;cin=0;
+        @(posedge clk) cin=1;
+        @(posedge clk) a=16'hCD;b=16'hFC;
+        @(posedge clk) cin=0;
+        @(posedge clk) a=16'h33;b=16'h77;
+        @(posedge clk) cin=1;
+        #20 $finish;
+    end
+endmodule
+
+module AdderBlock64bit(sum,cout,a,b,cin,clk);
+parameter integer SIZE = 16; //size >= 4;
+    output [SIZE-1:0]sum;
     output cout;
-    input [size-1:0]a,b;
+    input [SIZE-1:0]a,b;
     input cin;
-    wire [size>>2:0]carry;
+    input [SIZE-1:0]clk;
+    wire [SIZE>>2:0]carry;
 genvar i;
 generate
-    for(i=0;i<size;i=i+4)
+    for(i=0;i<SIZE;i=i+4)
     begin
         assign carry[0]=cin;
-        AdderBlock4bit blk1(.sum(sum[i+:4]),.cout(carry[(i>>2)+1]),.a(a[i+:4]),.b(b[i+:4]),.cin(carry[i>>2]));
-        assign cout=carry[size>>2];
+        AdderBlock4bit blk1(
+            .a(a[i+:4]), .b(b[i+:4]),.cin(carry[i>>2]),
+            .sum(sum[i+:4]),.cout(carry[(i>>2)+1]),
+            .clk(clk[i+:4])
+        );
+        assign cout=carry[SIZE>>2];
     end
 endgenerate
 endmodule
 
-module AdderBlock4bit(sum,cout,a,b,cin);
+module AdderBlock4bit(sum,cout,a,b,cin,clk);
     output [3:0]sum;
     output cout;
     input [3:0]a,b;
     input cin;
+    input [3:0] clk;
     wire [2:0]carry;
-AdderBlock1bit blk1 [3:0](.sum(sum),.cout({cout,carry}),.a(a),.b(b),.cin({carry,cin}));
+AdderBlock1bit blk1 [3:0](.sum(sum),.cout({cout,carry}),.a(a),.b(b),.cin({carry,cin}), .clk(clk));
 endmodule
 
-module AdderBlock1bit(sum,cout,a,b,cin);
+module AdderBlock1bit(sum,cout,a,b,cin, clk);
     output sum,cout;
     input a,b,cin;
     wire p;
-CarryGen1bit stat1(.cout(cout),.p(p),.a(a),.b(b),.cin(cin));
+    input clk;
+carrygen1bit stat1(.cout(cout),.p(p),.a(a),.b(b),.cin(cin), .clk(clk));
 PFA1bit add1(.sum(sum),.p(p),.cin(cin));
 endmodule
 
-module CarryGen1bit(cout,p,a,b,cin,clk);
+module carrygen1bit(cout,p,a,b,cin,clk);
     output cout,p;
     input a,b,cin,clk;
     supply1 vdd;
@@ -59,3 +91,4 @@ module PFA1bit(sum,p,cin);
     input p,cin;
 xor x1(sum,p,cin);
 endmodule
+
