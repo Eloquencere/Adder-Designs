bb72a6a58772288ad2aa7b7f9d3974be5a551248
Eloquencere <sriranga.jaguar@gmail.com>
2024-10-06 16:41:00 +0530

    update

--- Testbenches/UVM-Testbench/components/top/top.sv
+++ Testbenches/UVM-Testbench/components/top/top.sv
@@ -4,7 +4,7 @@ import uvm_pkg::*;
 
 module top;
     for(genvar i = 0; i < 1; i++) begin
-        adder_interface intrf ();
+        adder_interface intrf();
         case(i)
             0: CIAxbit DUT0(.sum(intrf.sum), .cout(intrf.cout), .a(intrf.a), .b(intrf.b), .cin(intrf.cin));
         endcase
@@ -14,5 +14,9 @@ module top;
             );
     end
     initial begin
+        static adder_test_config tst_cfg = adder_test_config::type_id::create("tst_cfg");
+        uvm_config_db#(adder_test_config)::set(uvm_root::get(), "uvm_test_top", "tst_cfg", tst_cfg);
+        // $timeformat(-9, 2, " ns", 20);
+        run_test("adder_test");
     end
 endmodule
